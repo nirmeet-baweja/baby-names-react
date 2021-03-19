ffeb7e127e8ddbef6741c78d0fc55d1184ea3d0e
Nirmeet <nirmeet.baweja@gmail.com>
2021-03-19 20:37:06 +0000

    Level 1 Completed

--- src/BabyName.js
+++ src/BabyName.js
@@ -2,8 +2,9 @@ import React from "react";
 // import BabyNamesData from "./data/babyNamesData.json";
 
 const BabyName = (props) => {
-  
-  return <p>Name : {props.babyName.name}</p>;
+  let cssClass = "";
+  props.babyName.sex === "f" ? (cssClass = "female") : (cssClass = "male");
+  return <span className={cssClass + " name"}>{props.babyName.name}</span>;
 };
 
 export default BabyName;
