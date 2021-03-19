9dc3f36529ed8dc047e72d0aefb37b9b2eb4c516
Nirmeet <nirmeet.baweja@gmail.com>
2021-03-19 19:55:49 +0000

    Test the json data file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BabyName from "./BabyName";
+import BabyNamesData from "./data/babyNamesData.json";
+
+import "./App.css";
 
 function App() {
+  console.log(BabyNamesData[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Baby Names</header>
+      {BabyNamesData.map((name) => (
+        <BabyName babyName={name} />
+      ))}
     </div>
   );
 }
---
+++ src/BabyName.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import BabyNamesData from "./data/babyNamesData.json";
+
+const BabyName = (props) => {
+  
+  return <p>Name : {props.babyName.name}</p>;
+};
+
+export default BabyName;
